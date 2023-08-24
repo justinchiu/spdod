15d9cae976d8a03791feeabef50a15db888e5717
Justin Chiu <justinchiu@g.harvard.edu>
2023-08-24 00:31:34 -0400

    add violin plot for matching baseline

--- spdod/play.py
+++ spdod/play.py
@@ -1,28 +1,55 @@
 
 from dialop.envs import OptimizationEnv
 from scipy.optimize import linear_sum_assignment as lsa
+import seaborn as sns
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import matplotlib
+
+
+NUM_TRIALS = 100
 
 env = OptimizationEnv()
-env.reset()
-game = env.game
 
-# ground truth
-table = game.table
-masks = game.masks
-scales = game.scales
-values = table.values
+both_ratios = []
+max_ratios = []
+for n in range(NUM_TRIALS):
+    env.reset()
+    game = env.game
 
-best_reward = game.best_assignment_reward
+    # ground truth
+    table = game.table
+    masks = game.masks
+    scales = game.scales
+    values = table.values
 
-# observed
-tables = game.tables
+    best_reward = game.best_assignment_reward
 
-def score(table, values):
-    rows, cols = lsa(values)
-    assignment = list(zip(rows, cols))
-    score = table.score(assignment)
-    return score
+    # observed
+    tables = game.tables
 
-max_value = lsa(values, maximize=True)
-value0 = lsa(values * masks[0], maximize=True)
-import pdb; pdb.set_trace()
+    def score(table, values):
+        rows, cols = lsa(values)
+        assignment = list(zip(rows, cols))
+        score = table.score(assignment)
+        return score
+
+    max_value = values[lsa(values, maximize=True)].sum()
+    value0 = values[lsa(values * masks[0], maximize=True)].sum()
+    value_both = values[lsa(values * (masks[0] | masks[1]), maximize=True)].sum()
+
+    print(n)
+    print(value0 / value_both)
+    print(value0 / max_value)
+    both_ratios.append(value0 / value_both)
+    max_ratios.append(value0 / max_value)
+    #import pdb; pdb.set_trace()
+    
+df = pd.DataFrame({"Max ratio": max_ratios, "Both ratio": both_ratios})
+
+# Create violin plot
+sns.violinplot(data=df)
+
+# Save the plot
+plt.savefig("figures/max_both_ratios.png")
